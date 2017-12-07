b67698b59ef5153c3e79b02e88b63a9f7cb1251a
liupan <13718051767@139.com>
2017-12-07 16:31:18 +0800

    add banner

---
+++ js/banner.js
@@ -0,0 +1,171 @@
+(function () {
+    window.onload = function () {
+        var banner = document.querySelector(".banner-wrap");
+        var list = document.querySelector(".banner-wrap>.banner-list");
+        var rootWidth = -document.documentElement.clientWidth;
+        var startX = 0;
+        var index = 0;
+        var translateX = 0;
+        var intId = null;
+        var tid = null;
+        var lis = null;
+        var points = null;
+        var startPoint = {};
+        var started = false;
+
+        init();
+        autoPlay();
+
+        //手指按下
+        banner.addEventListener('touchstart', function (ev) {
+            //禁止浏览器默认滑动事件
+            ev.preventDefault();
+
+            startX = ev.changedTouches[0].clientX;
+            const x = ev.changedTouches[ 0 ].clientX;
+            const y = ev.changedTouches[ 0 ].clientY;
+            
+            startPoint = { x, y };
+
+            //关闭过渡效果
+            list.style.transition = "0s";
+
+            //关闭自动轮播
+            window.clearTimeout(tid);
+            window.clearInterval(intId);
+
+            //无缝滑屏 改变index
+            changeIndex();
+        });
+
+        //手指滑动
+        banner.addEventListener('touchmove', function (ev) {
+            const x = ev.changedTouches[ 0 ].clientX;
+            const y = ev.changedTouches[ 0 ].clientY;
+
+            if( !started ) {
+                if( Math.abs( y - startPoint.y ) > Math.abs( x - startPoint.x ) ) {
+                    return;
+                }
+                started = true;
+            }
+
+            //滑动差值
+            var dis = ev.changedTouches[0].clientX - startX;
+            ev.preventDefault();
+            //当前list总平移长度
+            translateX = rootWidth * index + dis;
+
+            //响应list滑动平移
+            changePage(0, translateX);
+        });
+
+        //手指抬起
+        banner.addEventListener('touchend', function (ev) {
+            if( !started ) {
+                return;
+            }
+            started = false;
+            //根据滑动长度求索引
+            // index = Math.round(translateX / rootWidth);
+            var dis = ev.changedTouches[0].clientX - startX;
+            const r = Math.abs(dis);
+            if ( dis > 0 && r > 50 ) {
+                index--;
+            }
+            if ( dis < 0 && r > 50 ) {
+                index++;
+            }
+
+            //越界判断
+            if (index < 0) {
+                index = 0;
+            } else if (index > lis.length - 1) {
+                index = lis.length - 1;
+            }
+
+            changePage(.3, rootWidth * index);
+            changePoint();
+            if ( dis >=0 ) {
+                autoPlay('+');
+            } else {
+                autoPlay('-');
+            }
+        });
+
+        //自动轮播
+        function autoPlay(diretory = '-') {
+            console.log('tid', intId, tid);
+            intId = setInterval(function () {
+                changeIndex();
+                changePage(0, rootWidth * index);
+
+                //延时执行，为了让页面切换完毕
+                tid = setTimeout(function () {
+                    if (diretory === '-') {
+                        index++;
+                    } else {
+                        index--;
+                    }
+                    changePage(.3, rootWidth * index);
+                    changePoint();
+                }, 500);
+
+            }, 3000);
+        }
+
+        //更改索引 实现无缝滑屏
+        function changeIndex() {
+            if (index == 0) {
+                //当显示第一张图片 切换到下一组的第一张
+                index = points.length;
+            } else if (index == lis.length - 1) {
+                //当显示最后一张 切换到上一组的最后一张
+                index = points.length - 1;
+            }
+        }
+
+        //设置小圆点
+        function changePoint() {
+            for (var i = 0; i < points.length; i++) {
+                points[i].classList.remove('active');
+            }
+            points[index % (points.length)].classList.add('active');
+        }
+
+        //滑动页面 平移list
+        function changePage(duration, translateVal) {
+            list.style.transition = duration + "s";
+            list.style.transform = "translateX(" + translateVal + "px)";
+        }
+
+        //初始化list和point
+        function init() {
+            //添加一组
+            list.innerHTML += list.innerHTML;
+
+            //设置list宽度
+            lis = document.querySelectorAll("#banner-wrap>#banner-list li");
+            list.style.width = lis.length + "00%";
+
+            //设置li宽度
+            for (var i = 0; i < lis.length; i++) {
+                lis[i].style.width = 100 / lis.length + "%";
+            }
+
+            //创建point div
+            var pointDiv = document.createElement("div");
+            pointDiv.setAttribute("id", "point");
+            pointDiv.setAttribute("class", "point");
+            for (var i = 0; i < lis.length / 2; i++) {
+                var span = document.createElement("span");
+                if (i == 0) {
+                    span.classList.add('active');
+                }
+                pointDiv.appendChild(span);
+            }
+            banner.appendChild(pointDiv);
+            points = document.querySelectorAll("#banner-wrap>#point span");
+        }
+    };
+})();
